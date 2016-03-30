e88ad9d406e2e92dcc611178a7cfd1d8b3cd50da
akapoorx00 <aadit.kapoor@outlook.com>
2016-03-30 18:18:05 +0530

    a

--- torz/settings.py
+++ torz/settings.py
@@ -93,7 +93,7 @@ if development_flag:
         'USER': 'admin',
         'PASSWORD': 'silverjk',
         'HOST': 'localhost',
-        'PORT': '',
+        'PORT': '5432',
     }
     }
 else:
@@ -147,7 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_DIR, "torz/static")
+STATIC_ROOT = os.path.join(PROJECT_DIR, "staticfiles")
 
 
 STATICFILES_DIRS = [
